55b0833033a466c15d832988a3e6c82cea841823
Nano <emilianormz2706@gmail.com>
2026-09-06 19:11:09 -0600

    Added reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -16,3 +16,8 @@ function checkCount() {
     else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");}
 }
+
+function resetCount() {
+    count = 0;
+    alert("Your followers count has been reset to 0.");
+}
